706763dc9846071dc5fd3a8d7e17f96707ebc39b
AxureID <156453540+cristianT2002@users.noreply.github.com>
2024-12-02 11:06:15 -0500

    ll

--- Prueba2Camaras6NPTS copy 2.py
+++ Prueba2Camaras6NPTS copy 2.py
@@ -681,7 +681,7 @@ def gestionar_tiempos_npt(hora_primera_deteccion_segundos_almacenado, hora_sin_d
                     if not en_horario_desayuno and not en_horario_almuerzo:
                         # Clasificar el tiempo según duración
                         if tiempo_sin_deteccion > 0:
-                            if 2 * 60 <= tiempo_sin_deteccion <= 4 * 60:  # Parada corta
+                            if tiempo_sin_deteccion <= 2 * 60:  # Parada corta
                                 # Solo registrar si no se ha registrado ya
                                 if not registrado_parada_corta:
                                     contador_paradas_cortas += 1
@@ -978,4 +978,3 @@ if __name__ == "__main__":
     proceso_grabacion2.join()
  
     print("Ambos procesos han terminado.")
-
